6c307b8c4fbea648bcb03d37db414cb751e827ab
Chris Crane <chrisjcrane@gmail.com>
2023-05-30 15:32:05 -0600

    add associations to tea

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,7 @@
 class Tea < ApplicationRecord
+  has_many :subscriptions
+  has_many :customers, through: :subscriptions
+
   validates_presence_of :title
   validates_presence_of :description
   validates_presence_of :temperature
