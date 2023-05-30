d444e0b28eb77e91db518de611531dcd538f569d
Chris Crane <chrisjcrane@gmail.com>
2023-05-30 17:24:55 -0600

    add endpoint for user subscriptions with passing test

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  def index
+    customer = Customer.find(params[:customer_id])
+    render json: SubscriptionSerializer.new(customer.subscriptions)
+  end
+  
   def create
     render json: SubscriptionSerializer.new(Subscription.create(subscription_params))
   end
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -10,6 +10,21 @@ RSpec.describe 'Subscriptions API' do
     @subscription = create(:subscription, customer_id: @customer.id, tea_id: @tea.id)
   end
 
+  it "see all of a customer’s subsciptions (active and cancelled)" do
+    @subscription2 = create(:subscription, customer_id: @customer.id, tea_id: @tea.id, status: "cancelled")
+    get "/api/v1/customers/#{@customer[:id]}/subscriptions"
+
+    expect(response).to be_successful
+    data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(data.count).to eq(2)
+    # require 'pry'; binding.pry
+    expect(data[0][:id]).to eq(@subscription[:id].to_s)
+    expect(data[0][:attributes][:status]).to eq("active")
+    expect(data[1][:id]).to eq(@subscription2[:id].to_s)
+    expect(data[1][:attributes][:status]).to eq("cancelled")
+  end
+
   it "subscribe a customer to a tea subscription" do
     expect(Subscription.count).to eq(1)
     params = { customer_id: @customer.id, tea_id: @tea.id, title: "Monthly", price: 10.00, status: "active", frequency: 1 }
