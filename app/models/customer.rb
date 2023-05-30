b8460a64dda4bc1ea4d6b6aa405243d3e85406b1
Chris Crane <chrisjcrane@gmail.com>
2023-05-30 15:32:44 -0600

    add associations to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
+  has_many :subscriptions
+  has_many :teas, through: :subscriptions
+
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
